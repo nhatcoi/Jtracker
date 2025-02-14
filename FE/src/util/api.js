f7674a40fb893eecfca8d591b0615c827c9b7098
coinhat <nhatcoifacebook@gmail.com>
2025-02-14 13:21:35 +0700

    fix: 401

--- FE/src/util/api.js
+++ FE/src/util/api.js
@@ -51,7 +51,7 @@ api.interceptors.request.use(
 api.interceptors.response.use(
     response => response,
     async error => {
-        if (error.response && error.response.status === 403) {
+        if (error.response && error.response.status === 401) {
             console.log("Access token hết hạn, đang refresh...");
             const newAccessToken = await refreshAccessToken();
 

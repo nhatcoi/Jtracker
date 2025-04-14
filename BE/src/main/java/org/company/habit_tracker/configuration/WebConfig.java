bfdce0979c41bf09975863d015b69ff1ebdf5d81
nhatcoi <nhatcoifacebook@gmail.com>
2025-04-14 12:33:04 +0700

    feat: add OPTIONS method to CORS configuration

--- BE/src/main/java/org/company/habit_tracker/configuration/WebConfig.java
+++ BE/src/main/java/org/company/habit_tracker/configuration/WebConfig.java
@@ -14,8 +14,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns(url, "https://*.ngrok-free.app")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOriginPatterns(url)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }

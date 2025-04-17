53ff3177d3c4b10efa1aa3ceee7f4fcaa8cebe39
nhatcoi <nhatcoifacebook@gmail.com>
2025-04-17 11:46:04 +0700

    feat: add new endpoints for habit logs management

--- FE/src/api/endpoints.js
+++ FE/src/api/endpoints.js
@@ -16,5 +16,11 @@ export const API_ENDPOINTS = {
         UPDATE: (id) => `/habits/${id}`,
         DELETE: (id) => `/habits/${id}`,
         LOG: (id) => `/habits/${id}/logs`
+    },
+    HABIT_LOGS: {
+        LIST: `/habit-logs`,
+        UPDATE_BY_HABIT: (habitId) => `/habit-logs/update/${habitId}`,
+        UPDATE_BY_ID: (habitLogId) => `/habit-logs/${habitLogId}`,
+        GET_COUNTS: (habitId) => `/habit-logs/status-counts/{habitId}`
     }
 };
